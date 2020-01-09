13a91e572ea85b4f6618e1858ef35d9b556891cc
bsztreha <bsztreha@pfnet.hu>
2020-01-09 22:24:35 +0100

    Issue #1 - Fix coding standard

--- profile/modules/pfremember/src/Controller/PfRememberUpdate.php
+++ profile/modules/pfremember/src/Controller/PfRememberUpdate.php
@@ -4,9 +4,23 @@ namespace Drupal\pfremember\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+ * Class PfRememberUpdate.
+ *
+ * @package Drupal\pfremember\Controller
+ */
 class PfRememberUpdate extends ControllerBase {
 
-  public function processHash($hash) {
+  /**
+   * Process given hash and update remember item.
+   *
+   * @param string $hash
+   *   Secret hash generated from remember.
+   *
+   * @return mixed
+   *   Build array.
+   */
+  public function processHash(string $hash) {
     $build['main'] = [
       '#type' => 'markup',
       '#markup' => $hash,
